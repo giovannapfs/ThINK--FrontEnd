3d5e8f5bcda77a30c710b9abc2a56324e3bb07bc
giovannapfs <giovanna.figueirasilva@gmail.com>
2023-11-16 17:21:11 -0300

    testando hotjar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import Router from './Router';
+import React, { useEffect } from 'react';
+import { hotjar } from 'react-hotjar';
 
 function App() {
+  useEffect(() => {
+    hotjar.initialize(3738750, 6);
+  }, []);
+
   return (
    <Router/>
   );
